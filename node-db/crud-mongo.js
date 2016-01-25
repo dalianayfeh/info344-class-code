359119ae0239a7682bc0d3d11e78340458ec0623
Dalia Nayfeh <dnayfeh@uw.edu>
2016-01-25 10:19:17 -0800

    commit

--- node-db/crud-mongo.js
+++ node-db/crud-mongo.js
@@ -20,11 +20,31 @@ mongoose.connection.on('error', function(err) { // check if there was a problem 
 var newStory = {
     url: 'http://www.google.com'
 };
+var id;
 
-Story.create(newStory)
+Story.create(newStory) //INSERT
     .then(function(story) {
+        id = story._id;
         console.log('inserted new story!')
         console.log(story);
+    })
+    .then(function() {
+        return Story.findById(id).exec; // finds id. returns a query object
+    })
+    .then(function(story) {
+        console.log('found story!');
+        console.log(story);
+        
+        return Story.findByIdAndUpdate(id, {$inc: {votes: 1}}, {new: true}); // update any property in a story
+    })
+    .then(function(story) {
+        console.log('updated story!');
+        console.log(story);
+        
+        return Story.findByIdAndRemove(id); // delete
+    })
+    .then(function() {
+        console.log('story deleted!');
     })
     .then(null, function(err) { // use this instead of catch
         console.error(err);
